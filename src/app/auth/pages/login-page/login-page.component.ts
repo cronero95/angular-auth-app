128644b3b4ba4363c6266855f61029f65323a790
cronero95 <49892098+cronero95@users.noreply.github.com>
2024-04-04 22:47:01 -0500

    Add html to login page and auth layout
    Implement reactive form in login page

--- src/app/auth/pages/login-page/login-page.component.ts
+++ src/app/auth/pages/login-page/login-page.component.ts
@@ -1,9 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   templateUrl: './login-page.component.html',
   styleUrl: './login-page.component.css'
 })
 export class LoginPageComponent {
+
+  private formBuilder = inject(FormBuilder);
+
+  public myForm: FormGroup = this.formBuilder.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required, Validators.minLength(6)]],
+  });
+
+  login(): void {
+    console.log(this.myForm.value);
+
+  }
 
 }
