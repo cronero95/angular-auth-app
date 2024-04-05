945b161867a6ffbdeab906119d11bce94a300fc6
cronero95 <49892098+cronero95@users.noreply.github.com>
2024-04-05 11:40:03 -0500

    Make login in our backend from our frontend

--- src/app/auth/pages/login-page/login-page.component.ts
+++ src/app/auth/pages/login-page/login-page.component.ts
@@ -1,5 +1,6 @@
 import { Component, inject } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   templateUrl: './login-page.component.html',
@@ -8,6 +9,7 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 export class LoginPageComponent {
 
   private formBuilder = inject(FormBuilder);
+  private authService = inject(AuthService);
 
   public myForm: FormGroup = this.formBuilder.group({
     email: ['', [Validators.required, Validators.email]],
@@ -16,6 +18,13 @@ export class LoginPageComponent {
 
   login(): void {
     console.log(this.myForm.value);
+
+    const { email, password } = this.myForm.value;
+    this.authService.login(email, password)
+      .subscribe(success => {
+        console.log({success});
+
+      })
 
   }
 
--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -1,6 +1,6 @@
-import { Injectable, inject, signal } from '@angular/core';
+import { Injectable, computed, inject, signal } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable, of } from 'rxjs';
+import { Observable, map, of, tap } from 'rxjs';
 
 import { environment } from '../../../environments/environments';
 
@@ -17,10 +17,27 @@ export class AuthService {
   private _currentUser = signal<User|null>(null);
   private _authStatus = signal<AuthStatus>(AuthStatus.checking);
 
+  public currentUser =  computed(() => this._currentUser());
+  public authStatus = computed(() => this._authStatus());
+
   constructor() { }
 
   login(email: string, password: string): Observable<boolean> {
 
-    return of(true);
+    const url = `${this.baseUrl}/auth/login`;
+    const body = { email, password };
+
+    return this.httpClient.post<LoginResponse>(url, body)
+      .pipe(
+        tap(({user, token}) => {
+          this._currentUser.set(user);
+          this._authStatus.set(AuthStatus.authenticated);
+          localStorage.setItem('token', token);
+
+          console.log({user, token});
+
+        }),
+        map(() => true)
+      );
   }
 }
